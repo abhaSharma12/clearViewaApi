1819c846dc37e5189460063dd64149c6dd915289
Your Name <you@example.com>
2018-09-04 12:06:44 +0530

    6x files are deleted

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var logger= require('./config/logger').logger;
 try {
 	var sendAgentSummaryData = require('./controllers/sendAgentSummaryData');
 	var sendSkillGroupSummaryData = require('./controllers/sendSkillGroupSummaryData');
-	var sendAgentSummaryData_6x = require('./controllers/sendAgentSummaryData_6x');
-	var sendSkillGroupSummaryData_6x = require('./controllers/sendSkillGroupSummaryData_6x');
+	
+
 
 	var express = require('express');
 	var app = express();
@@ -28,22 +28,8 @@ try {
 		});
 	});
 
-	router.post('/sendAgentSummaryData_6x', function (req, res) {
-		logger.info("Calling sendAgentSummaryData_6x at " + new Date());
-		sendAgentSummaryData_6x.send(function(response) {
-			logger.info("sendAgentSummaryData_6x - sending response as : " + response);
-			res.send(response);
-		});
-		
-	});
-
-	router.post('/sendSkillGroupSummaryData_6x', function (req, res) {
-		logger.info("Calling sendSkillGroupSummaryData_6x at " + new Date());
-		sendSkillGroupSummaryData_6x.send(function(response) {
-			logger.info("sendSkillGroupSummaryData_6x - sending response as : " + response);
-			res.send(response);
-		});
-	});
+	
+	
 
 	app.use('/', router);
 	app.listen(port);
